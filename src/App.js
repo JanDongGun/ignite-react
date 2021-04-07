fa464e2f0953d53ef71eaa29ef05b02773ce6191
JanDongGun <donggontrannguyen@gmail.com>
2021-04-07 18:32:06 +0700

    fetching first data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gameAction";
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  });
   return (
     <div className="App">
       <h1>Ignite</h1>
